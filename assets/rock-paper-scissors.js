0393be427c0edb956d63f62a303b2925a7deef64
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2024-09-02 20:48:25 +0800

    added buttons for choices and js for player input

--- assets/rock-paper-scissors.js
+++ assets/rock-paper-scissors.js
@@ -3,18 +3,18 @@ let humanScore = 0;
 let computerScore = 0;
 
 // function for the player or human that converts the input to lowercase
-function getHumanChoice(humanInput) {
-  humanInput = humanInput.toLowerCase();
-  while (
-    humanInput !== "rock" &&
-    humanInput !== "paper" &&
-    humanInput !== "scissors"
-  ) {
-    alert("Please enter a valid option!");
-    humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
-  }
-  return humanInput;
-}
+// function getHumanChoice(humanInput) {
+//   humanInput = humanInput.toLowerCase();
+//   while (
+//     humanInput !== "rock" &&
+//     humanInput !== "paper" &&
+//     humanInput !== "scissors"
+//   ) {
+//     console.log("Please enter a valid option!");
+//     humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
+//   }
+//   return humanInput;
+// }
 
 // function for the computer that gives a random choice
 function getComputerChoice() {
@@ -36,7 +36,7 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice == "paper" && computerChoice == "rock") ||
     (humanChoice == "scissors" && computerChoice == "paper")
   ) {
-    return alert(
+    return console.log(
       `You win!\n${humanChoice} beats ${computerChoice}\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${++humanScore}\nComputer: ${computerScore}`
     );
   } else if (
@@ -44,45 +44,75 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice == "paper" && computerChoice == "scissors") ||
     (humanChoice == "scissors" && computerChoice == "rock")
   ) {
-    return alert(
+    return console.log(
       `You lose!\n${computerChoice} beats ${humanChoice}\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${humanScore}\nComputer: ${++computerScore}`
     );
   } else {
-    return alert(
+    return console.log(
       `It's a tie!\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${humanScore}\nComputer: ${computerScore}`
     );
   }
 }
 
-function playGame() {
+function playGame(input) {
   // get the input of the player
-  let humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
+  // let humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
 
   // assigning the functions' output into a variable
-  const humanSelection = getHumanChoice(humanInput);
+  // const humanSelection = getHumanChoice(input);
+  const humanSelection = input;
   const computerSelection = getComputerChoice();
 
   return playRound(humanSelection, computerSelection);
 }
 
-// loop that will repeat the in 5 rounds
-for (round = 1; round <= 5; round++) {
-  playGame();
-}
+// // loop that will repeat the in 5 rounds
+// for (round = 1; round <= 5; round++) {
+//   playGame();
+// }
 
 // conditional statement that will display who won the game based on the score
-if (humanScore > computerScore) {
-  alert(
-    `You won the game!\n\nFinal Score:\nYou: ${humanScore}\nComputer: ${computerScore}`
-  );
-} else if (computerScore > humanScore) {
-  alert(
-    `The computer won the game!\n\nFinal Score:\nYou: ${humanScore}\nComputer: ${computerScore}`
-  );
-} else if (humanScore == computerScore) {
-  alert(
-    `It's a tie\n\nFinal Score:\nYou: ${humanScore}\nComputer: ${computerScore}`
-  );
-} else {
-  alert("Something is not right");
+// if (humanScore > computerScore) {
+//   console.log(
+//     `You won the game!\n\nFinal Score:\nYou: ${humanScore}\nComputer: ${computerScore}`
+//   );
+// } else if (computerScore > humanScore) {
+//   console.log(
+//     `The computer won the game!\n\nFinal Score:\nYou: ${humanScore}\nComputer: ${computerScore}`
+//   );
+// } else if (humanScore == computerScore) {
+//   console.log(
+//     `It's a tie\n\nFinal Score:\nYou: ${humanScore}\nComputer: ${computerScore}`
+//   );
+// } else {
+//   console.log("Something is not right");
+// }
+
+//DOM elements for GUI
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const roundResult = document.querySelector(".round-result");
+
+rock.addEventListener("click", () => {
+  input = "rock";
+  playGame(input);
+});
+
+paper.addEventListener("click", () => {
+  input = "paper";
+  playRound(input);
+});
+
+scissors.addEventListener("click", () => {
+  input = "scissors";
+  playRound(input);
+});
+
+// function clickHandler(btnInput) {
+//   testMessage(btnInput);
+// }
+
+function testMessage(input) {
+  alert(input);
 }

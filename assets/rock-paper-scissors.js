73a5954f3aab3ac4d2b2a4d1b7d67f8d7c846689
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2024-09-02 11:21:40 +0800

    Edited the format of the rps code

--- assets/rock-paper-scissors.js
+++ assets/rock-paper-scissors.js
@@ -2,69 +2,70 @@
 let humanScore = 0;
 let computerScore = 0;
 
+// function for the player or human that converts the input to lowercase
+function getHumanChoice(humanInput) {
+  humanInput = humanInput.toLowerCase();
+  while (
+    humanInput !== "rock" &&
+    humanInput !== "paper" &&
+    humanInput !== "scissors"
+  ) {
+    alert("Please enter a valid option!");
+    humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
+  }
+  return humanInput;
+}
+
+// function for the computer that gives a random choice
+function getComputerChoice() {
+  let computerRandomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (computerRandomNumber === 3) {
+    return "rock";
+  } else if (computerRandomNumber === 2) {
+    return "scissors";
+  } else {
+    return "paper";
+  }
+}
+
+// function that output a message and score based on the choices of the player and computer
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    return alert(
+      `You win!\n${humanChoice} beats ${computerChoice}\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${++humanScore}\nComputer: ${computerScore}`
+    );
+  } else if (
+    (humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")
+  ) {
+    return alert(
+      `You lose!\n${computerChoice} beats ${humanChoice}\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${humanScore}\nComputer: ${++computerScore}`
+    );
+  } else {
+    return alert(
+      `It's a tie!\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${humanScore}\nComputer: ${computerScore}`
+    );
+  }
+}
+
 function playGame() {
   // get the input of the player
   let humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
-
-  // function for the player or human that converts the input to lowercase
-  function getHumanChoice(humanInput) {
-    humanInput = humanInput.toLowerCase();
-    while (
-      humanInput !== "rock" &&
-      humanInput !== "paper" &&
-      humanInput !== "scissors"
-    ) {
-      alert("Please enter a valid option!");
-      humanInput = prompt(`What will you choose?\n- Rock\n- Paper\n- Scissors`);
-    }
-    return humanInput;
-  }
 
   // assigning the functions' output into a variable
   const humanSelection = getHumanChoice(humanInput);
   const computerSelection = getComputerChoice();
 
-  // function for the computer that gives a random choice
-  function getComputerChoice() {
-    let computerRandomNumber = Math.floor(Math.random() * 3) + 1;
-
-    if (computerRandomNumber === 3) {
-      return "rock";
-    } else if (computerRandomNumber === 2) {
-      return "scissors";
-    } else {
-      return "paper";
-    }
-  }
-
-  // function that output a message and score based on the choices of the player and computer
-  function playRound(humanChoice, computerChoice) {
-    if (
-      (humanChoice == "rock" && computerChoice == "scissors") ||
-      (humanChoice == "paper" && computerChoice == "rock") ||
-      (humanChoice == "scissors" && computerChoice == "paper")
-    ) {
-      return alert(
-        `You win!\n${humanChoice} beats ${computerChoice}\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${++humanScore}\nComputer: ${computerScore}`
-      );
-    } else if (
-      (humanChoice == "rock" && computerChoice == "paper") ||
-      (humanChoice == "paper" && computerChoice == "scissors") ||
-      (humanChoice == "scissors" && computerChoice == "rock")
-    ) {
-      return alert(
-        `You lose!\n${computerChoice} beats ${humanChoice}\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${humanScore}\nComputer: ${++computerScore}`
-      );
-    } else {
-      return alert(
-        `It's a tie!\n\nYou: ${humanChoice.toUpperCase()}\nComputer: ${computerChoice.toUpperCase()}\n\n(Score)\nYou: ${humanScore}\nComputer: ${computerScore}`
-      );
-    }
-  }
   return playRound(humanSelection, computerSelection);
 }
 
-// loop that will repeat the round until one of the players reach 5 points
+// loop that will repeat the in 5 rounds
 for (round = 1; round <= 5; round++) {
   playGame();
 }
